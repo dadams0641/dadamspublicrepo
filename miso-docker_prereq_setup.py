bda109f27d1f1bd01eb9645a3672ddaae5606230
David Adams <dadams0641@gmail.com>
2021-06-24 12:52:06 -0400

    tafads

--- miso-docker_prereq_setup.py
+++ miso-docker_prereq_setup.py
@@ -4,14 +4,15 @@ import os
 import sys
 import subprocess
 
+sudo_check = False
 print("This Script Requires Sudo. Checking.")
 sudo_check = subprocess.Popen('env | grep -i sudo_user', shell=True, stdout=subprocess.PIPE)
-try:
-    for sc in sudo_check.stdout:
+if sudo_check:
+        for sc in sudo_check.stdout:
         sc = sc.decode()
         sc = sc.rstrip()
         print("Successfully Running as Sudo. Continuing")
-except:
+else:
     print("Not Running as Sudo. Exiting.")
     sys.exit(1)
 
