044ca1aa5d0df99685ddb2075a91fa52e7998dad
David Adams <dadams0641@gmail.com>
2021-06-24 13:42:53 -0400

    Built miso-docker_prereq_setup for development.

--- miso-docker_prereq_setup.py
+++ miso-docker_prereq_setup.py
@@ -17,4 +17,17 @@ else:
     print("Not Running as Sudo. Exiting.")
     sys.exit(1)
 
+print("Running SNAP Installer for CMAKE Classic.")
+os.system("snap install cmake --classic")
 
+print("Running First APT Installs for GIT, GAWK, and ASCIIDOC-BASE (A2X).")
+os.system("apt update; apt install git gawk asciidoc-base -y")
+os.system("git clone --recursive https://github.com/reconquest/shdoc; cd shdoc; make install")
+
+print("Setting Up Miso Apt. This may take a while.")
+os.mkdir("/miso-apt")
+os.system("chown $USER: /miso-apt")
+os.system("gcsfuse -o ro,allow_other --implicit-dirs miso-apt /miso-apt")
+os.system('echo "deb [trusted=yes] file:/miso-apt/debs/amd64 ./" | tee /etc/apt/sources.list.d/miso-latest.list')
+os.system('apt update')
+os.system('apt install miso-docker-core miso-docker-help -y')
